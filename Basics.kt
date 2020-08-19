cdd2c43f23c05c827bf757b17584ef2277f4c44f
R AMARTYA <amartyas02@gmail.com>
2020-08-19 14:30:56 +0530

    Create Basics.kt

---
+++ Basics.kt
@@ -0,0 +1,23 @@
+//VARIABLES
+var language ="Java" // var language: String ="Java"  
+val salary = 30000   // val salary: Int = 30000  
+//var (Mutable variable): We can change the value of variable declared using var keyword later in the program.
+//val (Immutable variable): We cannot change the value of variable which is declared using val keyword.
+
+//TYPE CONVERSION
+var value1 = 10
+var value2:Long = value1.toLong() //toByte(), toShort(), toLong(), toInt(), toDouble(), toChar()
+
+//OPERATOR
+var a = 10
+println(a) //10
+println(+a) //10
+println(+ +a) //10
+println(+ -a) //-10
+println(+ ++a) //11
+
+//OUTPUT
+println(10)  
+println("Hello World")  //println prints and mives to next line
+print(20)  
+print("Hello Universe")  //print prints and remains in the same line
