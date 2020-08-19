d91053a756f4af4cccd8e57432df36d7f59eebd5
R AMARTYA <amartyas02@gmail.com>
2020-08-19 12:14:41 +0530

    Create Array.kt

---
+++ Array.kt
@@ -0,0 +1,26 @@
+fun main() {
+//   var a: Int = 10
+//   var b: Char = 'A'
+//   var letter: Boolean = true
+//   var word: String = "name"
+//   var array_1: IntArray = intArrayOf(1,2,3)
+//   println(b)
+//   println(a)
+//   println(letter)
+//   println(word)
+//   print(array_1[0])
+  var array_2 = arrayOf(1,"this",true)
+  print(array_2[1])
+  print("\n")
+//   array_2.set(0, 2)
+//   var array_3 = arrayOf<Int>(1,2,3)
+//   array_3.set(0, 99)
+//   array_3[0]=99
+//   print(array_3[0])
+  
+//   var array_4 = arrayOf<String>("a", "b", "c")
+//   print(array_4[0])
+  var zero_array = Array<Int>(5){0}  '''Array of zeroes'''
+	
+  
+}
